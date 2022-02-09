a6ee9b5dddc9e345b77d6ad0b712609a83385554
pdeveau1 <pdeveau@bu.edu>
2022-02-09 19:51:36 +0000

    Parse command after user enters input

--- simple_shell/myshell_simpleshell.c
+++ simple_shell/myshell_simpleshell.c
@@ -15,12 +15,24 @@ void user_prompt(char *in_command)
 void run_shell()
 {
     char in_command[MAX_LINE_LENGTH];
+    struct pipeline *pipe;
     //run until user enters CTRL + D
     //CTRL + D returns EOF
     do
     {
+        //prompt and get input from user
         user_prompt(in_command);
+        //parse the command
+        pipe = pipeline_build(in_command);
+        //Execute the command
+        
+        
+        
+        
+        
+        pipeline_free(pipe);
     }while(!feof(stdin));
+    
     printf("\n");
 }
 
