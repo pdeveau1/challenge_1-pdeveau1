45c7d42bb6a4548d366f06b1faa33d1e6b21962f
pdeveau1 <pdeveau@bu.edu>
2022-02-12 00:05:23 +0000

    initial code for handling files for redirect in and out

--- simple_shell/.ipynb_checkpoints/myshell_simpleshell-checkpoint.h
+++ simple_shell/.ipynb_checkpoints/myshell_simpleshell-checkpoint.h
@@ -10,8 +10,19 @@
 //prompts user for command input
 void user_prompt(char*);
 
-//In this case, a command takes its input from the file (not stdin). Note that spacing is irrelevant around the < operator. For example, ​cat<file​ and ​cat <file​ are valid inputs. Also, only one input redirection is allowed for a single command. (​cat<<file ​is invalid)
+void execute_command(struct pipeline_command *);
+
+//In this case, a command takes its input from the file (not stdin). Note that spacing is irrelevant around the < operator. For example, cat<file and cat <file are valid inputs. Also, only one input redirection is allowed for a single command. (cat<<file is invalid)
 void execute_redirect_in(char*);
+
+//An input following this template indicates that a command writes its output to the specified file (not stdout). Again, spacing is irrelevant (see case a) and only one input redirection is allowed for a single command.
+void execute_redirect_out(char*);
+
+//The pipe character allows several commands to be connected, forming a pipeline. The output of the command before “|” is piped to the input of the command following “|”. A series of multiple piped commands is allowed on the command line. Spacing is irrelevant (described above). Example: cat a| sort | wc indicates that the output of the cat command is channeled to the sort and sort sends its output to the input of the wc program.
+void execute_pipeline(char*);
+
+//The ampersand character '&’ should allow the user to execute a command (or pipeline of commands) in the background. In this case, the shell immediately displays a prompt for the next line regardless of whether the commands on the previous line have finished or are still in progress
+void execute_background(char*);
 
 //overarching function to run shell processes
 void run_shell();
--- simple_shell/myshell_simpleshell.c
+++ simple_shell/myshell_simpleshell.c
@@ -7,6 +7,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+
+#define TOK_DELIM " \t\r\n"
+#define RED "\033[0;31m"
+#define RESET "\e[0m"
 
 //https://stackoverflow.com/questions/7271939/warning-ignoring-return-value-of-scanf-declared-with-attribute-warn-unused-r/13390863
 static inline void ignore_ret() {}
@@ -17,31 +22,70 @@ void user_prompt(char *in_command)
     ignore_ret(fgets(in_command, MAX_LINE_LENGTH, stdin));
 }
 
-void execute_command(char** command)
+void execute_command(struct pipeline_command * commands)
 {
     pid_t pid = fork(); //return value: -1 = failed, 0 = in child process, positive = in parent process
+    int status;
     char path[MAX_LINE_LENGTH];
-    //char *envp[] = {(char*) "PATH=/bin", 0};
-     //Given in class slides
-    if(pid == -1) //process failed
+    
+    int in = execute_redirect_in(commands->redirect_in_path);
+    int out = execute_redirect_out(commands->redirect_out_path);
+    
+    if (pid == 0) 
     {
-        
+        if (execvp(commands->command_args[0], commands->command_args) < 0)
+        {
+          printf("dash: command not found: %s\n", commands->command_args[0]);
+        }
+        //after execution, revert back to stdin and stdout
+        dup2(in, 0);
+        dup2(out, 1);
+        close(in);
+        close(out);
+        exit(EXIT_FAILURE);
     }
-    else if(pid ==0) //parent process
+    else if (pid < 0)
     {
-        //waitpid(-1, &status, 0); //wait for child to exit
-        wait(NULL);
+        printf(RED "Error forking" RESET "\n");
     }
-    else //child process
+    else 
     {
-        close(0); //close stdin
-        close(1); //close stdout
-        strcpy(path, "/bin/");
-        strcat(path, command[0]);
-        //execute command
-        //execve(cmd, command, envp); 
-        execvp(path, command);
+        waitpid(pid, & status, WUNTRACED);
     }
+}
+
+int execute_redirect_in(char* inpath)
+{
+    int tmp = dup(0);
+    int in;
+    if(inpath)
+    {
+        in = open(inpath, O_RDONLY); //open input file
+    }
+    else
+    {
+        in = dup(tmp); //duplicate stdin for input file
+    }
+    dup2(in,0);
+    close(in);
+    return tmp;
+}
+
+int execute_redirect_out(char* outpath)
+{
+    int tmp = dup(1);
+    int out;
+    if(outpath)
+    {
+        out = open(outpath,O_WRONLY);
+    }
+    else
+    {
+        out = dup(tmp);
+    }
+    dup2(out,1);
+    close(out);
+    return tmp;
 }
 
 void run_shell()
@@ -54,15 +98,18 @@ void run_shell()
     
     //run until user enters CTRL + D
     //CTRL + D returns EOF
-    while(!feof(stdin))
+    //while(!feof(stdin))
     {
         
         //parse the command
         pipe = pipeline_build(in_command);
+        
         //Execute the command
-        execute_command(pipe->commands->command_args);
+        execute_command(pipe->commands);
+        
         //free memory for pipe
         pipeline_free(pipe);
+        
         //prompt and get input from user
         user_prompt(in_command);
     }
