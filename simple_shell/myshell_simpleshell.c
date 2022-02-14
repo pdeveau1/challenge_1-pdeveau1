c4993361d3353de6185f1f47d98e042c8415e460
pdeveau1 <pdeveau@bu.edu>
2022-02-14 22:47:04 +0000

    Try new idea of loop that handles pipe and redirect, no functions for each

--- simple_shell/myshell_simpleshell.c
+++ simple_shell/myshell_simpleshell.c
@@ -26,17 +26,110 @@ int user_prompt(char* command)
     }
 }
 
-void execute_command(struct pipeline *pipe_line)
+void execute_pipeline(struct pipeline *pipe_line)
 {
     struct pipeline_command *commands = pipe_line->commands;
-    bool background = pipe_line->is_background;
+    //bool background = pipe_line->is_background;
+    int std_in, std_out;
+    std_in = dup(STDIN_FILENO);
+    std_out = dup(STDOUT_FILENO);
     
-    //loop through each command in the pipeline
-    while(commands->next != NULL)
+    
+    if(commands->next == NULL) //no pipeline
     {
-        
+        int pid = fork();
+        if(pid == 0) //child process
+        {
+            if(commands->redirect_in_path != NULL) //check if redirect to input
+            {
+                int in = open(commands->redirect_in_path, O_RDONLY);
+                dup2(in, STDIN_FILENO); //set stdin to input file
+                close(in); //close input file
+            }
+            if(commands->redirect_out_path != NULL) //check if redirect to output
+            {
+                int out = open(commands->redirect_out_path, O_WRONLY);
+                dup2(out, STDOUT_FILENO); //set stdout to output file
+                close(out); //close output file
+            }
+            if(execvp(commands->command_args[0], commands->command_args) < 0) //error executing command
+            {
+                exit(1);
+            }
+        }
     }
+    else //if pipeline
+    {
+        //loop through each command in the pipeline
+        int input = execute_command(commands, 0, 1, 0); //execute first command
+        while(commands->next != NULL)
+        {
+            input = execute_command(commands, input, 0, 0); //execute middle commands
+            commands = commands->next;
+        }
+        input = execute_command(commands, input, 0, 1); //execute last command
+    }
+    dup2(std_in, STDIN_FILENO);
+    dup2(std_out, STDOUT_FILENO);
 }
+//write to stdout, '>', 1
+//read from stdin, '<', 0
+int execute_command(struct pipeline_command *commands, int in_pipe, int first, int last)
+{
+    //char* command = commands->command_args;
+    int in = in_pipe; //current file descriptor for input
+    int pipe_inout[2]; //file descriptor for pipe in and out
+    int pid; //id of process
+    if(pipe(pipe_inout) == -1) //error piping
+    {
+        exit(1);
+    }
+    
+    pid = fork(); //fork process
+    
+    if(pid == 0) //in child process
+    {
+        //execute command of pipeline
+        if(first) //if first command of the pipeline
+        {
+            if(commands->redirect_in_path != NULL) //check if redirect to input
+            {
+                in = open(commands->redirect_in_path, O_RDONLY);
+                dup2(in, STDIN_FILENO); //set stdin to input file
+                close(in); //close input file
+            }
+            dup2(pipe_inout[1], STDOUT_FILENO); //process writes to next process 
+        }
+        else if(last) //if last command of the pipeline
+        {
+            if(commands->redirect_out_path != NULL) //check if redirect to output
+            {
+                int out = open(commands->redirect_out_path, O_WRONLY);
+                dup2(out, STDOUT_FILENO); //set stdout to output file
+                close(out); //close output file
+            }
+            dup2(in, STDIN_FILENO); //process reads from last process 
+        }
+        else //if a command in middle of pipeline
+        {
+            dup2(in, STDIN_FILENO); //process reads from last process
+            dup2(pipe_inout[1], STDOUT_FILENO); //process writes to next process
+        }
+        
+        if(execvp(commands->command_args[0], commands->command_args) < 0) //error executing command
+        {
+            exit(1);
+        }
+    }
+    close(pipe_inout[1]); //no longer need to write to pipe
+    if(last)
+    {
+        close(pipe_inout[0]); //no longer need to read from pipe
+    }
+    return pipe_inout[0]; //return read of pipe for next process
+    
+}
+
 void run_shell()
 {
     char command[MAX_LINE_LENGTH]; //stores command
@@ -47,7 +140,7 @@ void run_shell()
     {
         pipe_line = pipeline_build(command); //parse command entered
         
-        execute_command(pipe_line);
+        execute_pipeline(pipe_line);
         
         free(pipe_line); //free memory when done using pipeline
     }
--- simple_shell/myshell_simpleshell.h
+++ simple_shell/myshell_simpleshell.h
@@ -9,22 +9,7 @@
 
 //prompts user for command input
 int user_prompt(char*);
-
-void execute_command(struct pipeline *);
-
-//In this case, a command takes its input from the file (not stdin). Note that spacing is irrelevant around the < operator. For example, cat<file and cat <file are valid inputs. Also, only one input redirection is allowed for a single command. (cat<<file is invalid)
-int execute_redirect_in(char*);
-
-//An input following this template indicates that a command writes its output to the specified file (not stdout). Again, spacing is irrelevant (see case a) and only one input redirection is allowed for a single command.
-int execute_redirect_out(char*);
-
-//The pipe character allows several commands to be connected, forming a pipeline. The output of the command before “|” is piped to the input of the command following “|”. A series of multiple piped commands is allowed on the command line. Spacing is irrelevant (described above). Example: cat a| sort | wc indicates that the output of the cat command is channeled to the sort and sort sends its output to the input of the wc program.
-int execute_pipeline(struct pipeline_command*);
-
-//The ampersand character '&’ should allow the user to execute a command (or pipeline of commands) in the background. In this case, the shell immediately displays a prompt for the next line regardless of whether the commands on the previous line have finished or are still in progress
-void execute_background(char*);
-
-void reset_inout(int, int);
-//overarching function to run shell processes
+void execute_pipeline(struct pipeline *);
+int execute_command(struct pipeline_command *, int, int, int);
 void run_shell();
 #endif /* MYSHELL_SIMPLESHELL_H */
